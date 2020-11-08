fea9677db956104bee1459ec5df918952420dde0
Mya4ta <sefce3543@gmail.com>
2020-11-09 00:07:15 +0300

    add Tile type

---
+++ core/src/ru/terraria/type/Tile.java
@@ -0,0 +1,47 @@
+package ru.terraria.type;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Tile {
+    private Vector2 position;
+    private Rectangle bounds;
+    private Block block;
+    private Floor floor;
+
+    public Tile(Vector2 position) {
+        this.position = position;
+    }
+
+    public Tile() {
+        this.position = Vector2.Zero;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public Rectangle getBounds() {
+        return bounds;
+    }
+
+    public void setBounds(Rectangle bounds) {
+        this.bounds = bounds;
+    }
+
+    public Block getBlock() {
+        return block;
+    }
+
+    public void setBlock(Block block) {
+        this.block = block;
+    }
+
+    public Floor getFloor() {
+        return floor;
+    }
+
+    public void setFloor(Floor floor) {
+        this.floor = floor;
+    }
+}
--- core/src/ru/terraria/type/World.java
+++ core/src/ru/terraria/type/World.java
@@ -1,4 +1,5 @@
 package ru.terraria.type;
 
 public class World {
+
 }
